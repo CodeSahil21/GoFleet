7fdca8b668d146ee6a01273d4fd4083509bbf279
CodeSahil21 <sahil.s39026@email.com>
2025-02-15 13:47:25 +0530

    background clour red removed

---
+++ client/app/captain-Signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CaptainSignup: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello Captain - Signup</h1>
+        </div>
+    );
+};
+
+export default CaptainSignup;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-between w-full bg-red-500 pt-8 bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1611965008308-f117c8ca80fd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
+    <div className="h-screen flex flex-col justify-between w-full pt-8 bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1611965008308-f117c8ca80fd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
     >
       <Image
         className="w-16 ml-4"
